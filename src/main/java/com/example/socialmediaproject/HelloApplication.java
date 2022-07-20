59c80f6ffd081af28718868292a34580bdc1f887
matthewvines1 <matthewvines0819@gmail.com>
2022-07-20 09:12:30 -0500

    Started on tool banner.

--- src/main/java/com/example/socialmediaproject/HelloApplication.java
+++ src/main/java/com/example/socialmediaproject/HelloApplication.java
@@ -3,6 +3,10 @@ package com.example.socialmediaproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +15,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 960, 600);
         stage.setTitle("Hello!");
+        MenuBar menuBar = new MenuBar();
+
         stage.setScene(scene);
         stage.show();
     }
