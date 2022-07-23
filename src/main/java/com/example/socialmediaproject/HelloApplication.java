c0638b3afbe70a960c0fb5d7b63b84b91491497f
matthewvines1 <matthewvines0819@gmail.com>
2022-07-23 10:54:01 -0500

    New sub-sub menu

--- src/main/java/com/example/socialmediaproject/HelloApplication.java
+++ src/main/java/com/example/socialmediaproject/HelloApplication.java
@@ -22,40 +22,32 @@ public class HelloApplication extends Application {
         MenuBar menuBar = new MenuBar();
         //File
         Menu menu1 = new Menu("File");
-        MenuItem menuItem1_1 = new MenuItem("Placeholder 1");
-        menu1.getItems().add(menuItem1_1);
+        Menu menuItem1_1 = new Menu("Import");
+        MenuItem menuItem1_1_1 = new MenuItem("Import vCard");
+        MenuItem menuItem1_1_2 = new MenuItem("Import CSV");
+        menuItem1_1.getItems().addAll(menuItem1_1_1, menuItem1_1_2);
         MenuItem menuItem1_2 = new MenuItem("Placeholder 2");
-        menu1.getItems().add(menuItem1_2);
         MenuItem menuItem1_3 = new MenuItem("Placeholder 3");
-        menu1.getItems().add(menuItem1_3);
-        menuBar.getMenus().add(menu1);
+        menu1.getItems().addAll(menuItem1_1, menuItem1_2, menuItem1_3);
         //Edit
         Menu menu2 = new Menu("Edit");
         MenuItem menuItem2_1 = new MenuItem("Placeholder 1");
-        menu2.getItems().add(menuItem2_1);
         MenuItem menuItem2_2 = new MenuItem("Placeholder 2");
-        menu2.getItems().add(menuItem2_2);
         MenuItem menuItem2_3 = new MenuItem("Placeholder 3");
-        menu2.getItems().add(menuItem2_3);
-        menuBar.getMenus().add(menu2);
+        menu2.getItems().addAll(menuItem2_1, menuItem2_2, menuItem2_3);
         //View
         Menu menu3 = new Menu("View");
         MenuItem menuItem3_1 = new MenuItem("Placeholder 1");
-        menu3.getItems().add(menuItem3_1);
         MenuItem menuItem3_2 = new MenuItem("Placeholder 2");
-        menu3.getItems().add(menuItem3_2);
         MenuItem menuItem3_3 = new MenuItem("Placeholder 3");
-        menu3.getItems().add(menuItem3_3);
-        menuBar.getMenus().add(menu3);
+        menu3.getItems().addAll(menuItem3_1, menuItem3_2, menuItem3_3);
         //Help
         Menu menu4 = new Menu("Help");
         MenuItem menuItem4_1 = new MenuItem("Placeholder 1");
-        menu4.getItems().add(menuItem4_1);
         MenuItem menuItem4_2 = new MenuItem("Placeholder 2");
-        menu4.getItems().add(menuItem4_2);
         MenuItem menuItem4_3 = new MenuItem("About");
-        menu4.getItems().add(menuItem4_3);
-        menuBar.getMenus().add(menu4);
+        menu4.getItems().addAll(menuItem4_1, menuItem4_2, menuItem4_3);
+        menuBar.getMenus().addAll(menu1, menu2, menu3, menu4);
 
         VBox vBox = new VBox(menuBar);
         Scene scene = new Scene(vBox, 960, 900);
