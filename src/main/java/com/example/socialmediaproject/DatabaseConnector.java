02eda3fcb676d71b00d5138730c38cf0032d3e1b
matthewvines1 <matthewvines0819@gmail.com>
2022-08-25 20:08:08 -0500

    Got database working and tested.

--- src/main/java/com/example/socialmediaproject/DatabaseConnector.java
+++ src/main/java/com/example/socialmediaproject/DatabaseConnector.java
@@ -2,30 +2,33 @@ package com.example.socialmediaproject;
 
 import com.example.socialmediaproject.databaseentities.User;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.text.SimpleDateFormat;
 
 public class DatabaseConnector {
-    private Connection connection;
+
+    private final String SQL_GET_USER = "SELECT * FROM sys.users WHERE username=? AND password_hash=?";
+    private final String SQL_SET_USER = "INSERT INTO users (username, password_hash, has_view_role, " +
+            "has_edit_role, has_delete_role, is_active, creation_date_time, last_login_date_time) VALUES (" +
+            "?, ?, ?, ?, ?, ?, ?, ?)";
+    private String jdbcUrl;
+    private String sqlUsername;
+    private String sqlPassword;
     private User currentUser;
 
-    public DatabaseConnector(String jdbcURL, String username, String password) {
-        try (Connection newConnection = DriverManager.getConnection(jdbcURL, username, password)) {
-            connection = newConnection;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public DatabaseConnector(String jdbcUrl, String sqlUsername, String sqlPassword) {
+        this.jdbcUrl = jdbcUrl;
+        this.sqlUsername = sqlUsername;
+        this.sqlPassword = sqlPassword;
     }
 
     public User getUser(String username, String passwordHash) {
-        try(Statement statement = connection.createStatement()) {
-            String sql = "SELECT * FROM users WHERE username='"+username+"' AND password_hash='"+passwordHash+"'";
-                ResultSet resultSet = statement.executeQuery(sql);
-                while(resultSet.next()) {
+        try(Connection connection = DriverManager.getConnection(jdbcUrl, sqlUsername, sqlPassword);
+            PreparedStatement statement = connection.prepareStatement(SQL_GET_USER)) {
+            statement.setString(1, username);
+            statement.setString(2, passwordHash);
+            try(ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
                     currentUser = new User(username, passwordHash,
                             resultSet.getBoolean("has_view_role"),
                             resultSet.getBoolean("has_edit_role"),
@@ -37,6 +40,9 @@ public class DatabaseConnector {
                             resultSet.getTime("last_login_date_time"));
                     return currentUser;
                 }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -45,17 +51,21 @@ public class DatabaseConnector {
 
     public void setUser(User user) {
         if (currentUser.getIsActive() && currentUser.getEditRole()) {
-            try (Statement statement = connection.createStatement()) {
+            try (Connection connection = DriverManager.getConnection(jdbcUrl, sqlUsername, sqlPassword);
+                 PreparedStatement statement = connection.prepareStatement(SQL_SET_USER)) {
                 String creationDateTime = (new SimpleDateFormat("yyyy-MM-dd")).format(user.getCreationDate()) +
                         " " + (new SimpleDateFormat("HH:mm:ss")).format(user.getCreationTime());
                 String lastLoginDateTime = (new SimpleDateFormat("yyyy-MM-dd")).format(user.getLastLoginDate()) +
                         " " + (new SimpleDateFormat("HH:mm:ss")).format(user.getLastLoginTime());
-                String sql = "INSERT INTO users (name, username, password_hash, has_view_role, has_edit_role, has_delete_role, is_active, creation_date_time, last_login_date_time) " +
-                        "VALUES ('" + user.getUsername() + "', '" + user.getPasswordHash() + "', '" +
-                        (user.getViewRole() && currentUser.getViewRole()) + "', '" + (user.getEditRole()) + "', " +
-                        (user.getDeleteRole() && currentUser.getDeleteRole()) + "', '" + user.getIsActive() + "', " +
-                        creationDateTime + "', " + lastLoginDateTime;
-                statement.executeUpdate(sql);
+                statement.setString(1, user.getUsername());
+                statement.setString(2, user.getPasswordHash());
+                statement.setBoolean(3, user.getViewRole() && currentUser.getViewRole());
+                statement.setBoolean(4, user.getEditRole());
+                statement.setBoolean(5, user.getDeleteRole() && currentUser.getDeleteRole());
+                statement.setBoolean(6, user.getIsActive());
+                statement.setString(7, creationDateTime);
+                statement.setString(8, lastLoginDateTime);
+                statement.executeUpdate();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
